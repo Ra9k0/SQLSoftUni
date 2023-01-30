7c2a7de151b956b22a795ca9fcf4856aa7ed8e3d
Ra9k0 <rstambolijski@gmail.com>
2023-01-30 19:01:26 +0200

    .

--- 05.Subqueries and Joins/Subqueries and Joins.sql
+++ 05.Subqueries and Joins/Subqueries and Joins.sql
@@ -115,3 +115,66 @@ FROM
 [Employees] AS e 
 JOIN [Departments] AS d ON d.[DepartmentID] = e.[DepartmentID]
 JOIN [Employees] AS e2 ON e.[ManagerID] = e2.[EmployeeID]
+
+--PROBLEM 11
+SELECT
+MIN(avg) AS [MinAverageSalary]
+FROM(
+SELECT
+AVG([Salary]) AS avg
+FROM
+[Employees]
+GROUP BY [DepartmentID]) AS AverageSalary
+
+--PROBLEM 12
+SELECT
+mc.[CountryCode],
+m.[MountainRange],
+p.[PeakName],
+p.[Elevation]
+FROM
+[Peaks] AS p 
+JOIN [Mountains] AS m ON p.MountainId = m.Id
+JOIN [MountainsCountries] AS mc ON m.Id = mc.MountainId
+WHERE mc.CountryCode = 'BG' AND p.Elevation > 2835
+ORDER BY p.Elevation DESC
+
+--PROBLEM 13
+SELECT
+[CountryCode],
+COUNT([CountryCode]) AS [MountainRanges]
+FROM
+[MountainsCountries]
+WHERE [CountryCode] IN ('BG','RU','US')
+GROUP BY [CountryCode]
+
+--PROBLEM 14
+SELECT TOP 5
+c.[CountryName],
+r.[RiverName]
+FROM
+[CountriesRivers] AS cr
+RIGHT JOIN [Rivers] AS r ON r.Id = cr.RiverId
+RIGHT JOIN [Countries] AS c ON c.CountryCode = cr.CountryCode
+WHERE [ContinentCode] = 'AF'
+ORDER BY c.[CountryName]
+
+--PROBLEM 15
+SELECT [CC] FROM(
+SELECT 
+([CountryCode]) AS CC
+FROM
+[Countries] AS co
+GROUP BY [ContinentCode]) AS cc
+
+--PROBLEM 16
+SELECT c.[ContinentCode], c.[CurrencyCode], count(*)
+FROM [Countries] as c
+GROUP BY c.[ContinentCode], c.[CurrencyCode]
+HAVING count(*) > 1
+AND count(*) = (SELECT top 1 count(*)
+FROM [Countries] as csq
+WHERE csq.[ContinentCode] = c.[ContinentCode]
+GROUP BY csq.[ContinentCode], csq.[CurrencyCode]
+ORDER BY count(*) DESC)
+ORDER BY c.[ContinentCode]
